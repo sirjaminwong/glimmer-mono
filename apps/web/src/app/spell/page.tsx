8e56f84610720db4b30e3675208521566d631f7b
jamin <sirjaminwong@gmail.com>
2023-10-11 07:09:46 +0800

    feat(web): chore(web): 优化文件组织结构

--- apps/web/src/app/spell/page.tsx
+++ apps/web/src/app/spell/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useCallback, useMemo, useState } from "react";
-import WordChecker from "../components/word-checker";
+import WordChecker from "../../components/word-checker";
 import StatisticsBar from "./statistics-bar";
 
 const words: {
